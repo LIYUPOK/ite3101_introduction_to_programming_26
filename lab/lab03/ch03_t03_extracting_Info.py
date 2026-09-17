98a783b8d7c8e23bfa15e4f5fc859dc38418353e
LIYUPOK <liyupok@qq.com>
2026-09-17 08:05:25 +0000

    [skip actions] lab/lab03/ch03_t03_extracting_Info.py

--- lab/lab03/ch03_t03_extracting_Info.py
+++ lab/lab03/ch03_t03_extracting_Info.py
@@ -2,6 +2,6 @@ from datetime import datetime
 
 now = datetime.now()
 print(now)
-print (current_year = now.year)
-print (current_month = now.month)
-current_day = now.day
+print(current_year=now.year)
+print(current_month=now.month)
+print(current_day=now.day)
