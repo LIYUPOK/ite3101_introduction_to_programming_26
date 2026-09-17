51ef9d8d2f10e46a8bd85b7ff9ef32b8a55dfe02
LIYUPOK <liyupok@qq.com>
2026-09-17 08:49:04 +0000

    lab/lab04/ch04_t04_how_tables_have_turned.py

--- lab/lab04/ch04_t04_how_tables_have_turned.py
+++ lab/lab04/ch04_t04_how_tables_have_turned.py
@@ -7,10 +7,10 @@ bool_one = 3 < 5  # We already did this one for you!
 bool_two = 5 + 3 == 3 + 8
 
 # Make me true!
-bool_three = 3 == 6-2
+bool_three = 3 + 1 == 6 - 2
 
 # Make me false!
-bool_four = 6**6 <= 6 * 3
+bool_four = 6 ** 6 <= 6 * 3
 
 # Make me true!
 bool_five = 100 != 200
