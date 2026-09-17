6af354173721f0c346c5233429346c0a298255ca
LIYUPOK <liyupok@qq.com>
2026-09-17 07:21:30 +0000

    [skip actions] lab/lab02/ch02_t15_string_formatting_with_percentage_2.py

--- lab/lab02/ch02_t15_string_formatting_with_percentage_2.py
+++ lab/lab02/ch02_t15_string_formatting_with_percentage_2.py
@@ -4,4 +4,4 @@ color = input("What is your favorite color? ")
 
 # Uncomment the below 2 line of code!
 # print("Ah, so your name is %4, your quest is %5, "
-# "and your favorite color is ."  (name, quest, color))
+# "and your favorite color is %5."  (name, quest, color))
