169c0e25c6d99f4546a8fb66fb19ec47ee79fa88
LIYUPOK <liyupok@qq.com>
2026-09-24 08:40:08 +0000

    [skip actions] lab/lab05/ch05_t10_mytest.py

--- lab/lab05/ch05_t10_mytest.py
+++ lab/lab05/ch05_t10_mytest.py
@@ -2,7 +2,7 @@ pyg = 'ay'
 
 new_word = input('Enter a word:')
 if len(new_word) > 0 and new_word.isalpha():
-    word = original.lower()
+    word = new_word.lower()
     first = word[0]
     new_word = word + first + pyg
     new_word = new_word[1:len(new_word)]
