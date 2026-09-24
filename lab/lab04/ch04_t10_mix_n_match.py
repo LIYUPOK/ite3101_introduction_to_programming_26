15e83933640eb4908ea0fbcf58b22905142600cc
LIYUPOK <liyupok@qq.com>
2026-09-24 07:26:42 +0000

    [skip actions] lab/lab04/ch04_t10_mix_n_match.py

--- lab/lab04/ch04_t10_mix_n_match.py
+++ lab/lab04/ch04_t10_mix_n_match.py
@@ -7,7 +7,7 @@ bool_one = (2 <= 2) and "Alpha" == "Bravo"  # We did this one for you!
 bool_two = True and True
 
 # Make me false!
-bool_three = 3 ** 3 = 4 ** 4 
+bool_three = 3 ** 3 = 4 ** 4 or 
 
 # Make me true!
 bool_four = 10 / 2 = 2 + 3
