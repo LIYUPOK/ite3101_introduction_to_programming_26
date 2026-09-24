d7e3ecaa5068b39205f9091a00b652d6cfc94f36
LIYUPOK <liyupok@qq.com>
2026-09-24 08:41:38 +0000

    [skip actions] lab/lab05/ch05_t10_mytest.py

--- lab/lab05/ch05_t10_mytest.py
+++ lab/lab05/ch05_t10_mytest.py
@@ -5,6 +5,6 @@ if len(original) > 0 and original.isalpha():
     word = original.lower()
     first = word[0]
     original = word + first + pyg
-    original = original[1:len(original)]
+    original = original[1:len(new_word)]
 else:
     print('empty')
