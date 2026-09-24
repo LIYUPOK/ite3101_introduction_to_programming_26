61b71631834995bcaf0b7916f5672208ebe92002
LIYUPOK <liyupok@qq.com>
2026-09-24 08:41:52 +0000

    [skip actions] lab/lab05/ch05_t10_mytest.py

--- lab/lab05/ch05_t10_mytest.py
+++ lab/lab05/ch05_t10_mytest.py
@@ -4,7 +4,7 @@ original = input('Enter a word:')
 if len(original) > 0 and original.isalpha():
     word = original.lower()
     first = word[0]
-    original = word + first + pyg
-    original = original[1:len(new_word)]
+    new_word = word + first + pyg
+    new_word = new_word[1:len(new_word)]
 else:
     print('empty')
